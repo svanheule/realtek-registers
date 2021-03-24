svanheule/realtek-registers

commit cf58d6f86a9d082d5e4ddca8bdc7590df23c2c9c

Support user authentication via Github

realtek_reglist/controller.py
@@ -1,17 +1,49 @@
 from flask import abort, render_template, request, redirect, url_for
 from flask import Blueprint
-from flask_login import current_user
+from flask_dance.consumer import oauth_authorized
+from flask_dance.contrib.github import github
+from flask_login import current_user, login_user, logout_user
 
 from .models import db
+from .models.auth import User
 from .models.description import DescriptionRevision
 from .models.soc import Family, Feature, Register, Field, Table, TableField
+from .oauth import github_blueprint
 
 bp = Blueprint('realtek', __name__)
+
 
 @bp.route('/')
 def index():
     families = Family.query.order_by(Family.id).all()
     return render_template('index.html', families=families)
+
+@bp.route('/github')
+def login():
+    if not github.authorized:
+        return redirect(url_for('github.login'))
+    return redirect(url_for('realtek.index'))
+
+@bp.route('/logout/')
+def logout():
+    logout_user()
+    return redirect(url_for('realtek.index'))
+
+
+@oauth_authorized.connect_via(github_blueprint)
+def github_logged_in(blueprint, token):
+    info = blueprint.session.get('/user')
+    if info.ok:
+        account_info = info.json()
+        username = account_info['login']
+
+        query = User.query.filter_by(username=username)
+        try:
+            user = query.one()
+            login_user(user)
+        except NoResultFound:
+            pass
+    return redirect(url_for('realtek.index'))
 
 
 @bp.route('/<string:platform>/')

realtek_reglist/__init__.py
@@ -6,6 +6,7 @@ import os
 from .controller import bp
 from .models import db
 from .models.auth import login_manager
+from .oauth import github_blueprint
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///realtek-register.db'
@@ -18,6 +19,7 @@ db.init_app(app)
 login_manager.init_app(app)
 
 app.register_blueprint(bp, url_prefix='/realtek')
+app.register_blueprint(github_blueprint, url_prefix='/realtek/login')
 
 @app.template_filter('markdown')
 def markdown_filter(s):

realtek_reglist/oauth.py
@@ -1,0 +1,19 @@
+from flask import redirect
+from flask_dance.contrib.github import github, make_github_blueprint
+from flask_dance.consumer.storage.sqla import SQLAlchemyStorage
+from flask_login import current_user, login_user
+import os
+
+from .models import db
+from .models.auth import OAuth, User
+
+github_blueprint = make_github_blueprint(
+    client_id=os.getenv('GITHUB_ID'),
+    client_secret=os.getenv('GITHUB_SECRET'),
+    storage=SQLAlchemyStorage(
+        OAuth,
+        db.session,
+        user=current_user,
+        user_required=False,
+    ),
+)

realtek_reglist/templates/base.html
@@ -32,22 +32,35 @@ header > *::after {
 header > *:last-child::after {
     content: "";
 }
-header a {
+header a, footer a {
     text-decoration: none;
     font-weight: bold;
 }
 article {
     padding: 0 1em 1em 1em;
 }
-article a {
+article a, footer a {
     text-decoration: none;
     color: #3465a4;
 }
-article a:hover, article a:active {
+article a:hover, article a:active, footer a:hover, footer a:active {
     color: #f57900;
 }
-.overview thead {
-    font-size: larger;
+body > footer {
+    font-size: smaller;
+    position: fixed;
+    width: 100%;
+    bottom: 0;
+    left: 0;
+    z-index: 10;
+    text-align: right;
+    background: white;
+}
+body > footer p {
+    margin: 0.5em 1em 0.5em 0.2em;
+}
+body > footer a {
+    text-decoration: none;
 }
 .overview {
     margin: 0 auto;
@@ -55,6 +68,7 @@ article a:hover, article a:active {
     border-collapse: collapse;
 }
 .overview thead {
+    font-size: larger;
     background-color: white;
     position: sticky;
     top: 3em;
@@ -101,5 +115,12 @@ article a:hover, article a:active {
 {% block content %}
 {% endblock %}
 </article>
+<footer>
+<p>{% if current_user.is_authenticated %}
+{{ current_user.username }} <a href="{{ url_for('realtek.logout') }}">Log out</a>
+{% else %}
+<a href="{{ url_for('realtek.login') }}">Log in</a>
+{% endif %}</p>
+</footer>
 </body>
 </html>

commit 665e4609c293d8392d80f44859a43b13e2c7184b

Fix Github OAuth callback
Fixes: cf58d6f86a9d ("Support user authentication via Github")

realtek_reglist/controller.py
@@ -3,6 +3,7 @@ from flask import Blueprint
 from flask_dance.consumer import oauth_authorized
 from flask_dance.contrib.github import github
 from flask_login import current_user, login_user, logout_user
+from sqlalchemy.orm.exc import NoResultFound
 
 from .models import db
 from .models.auth import User
@@ -42,7 +43,8 @@ def github_logged_in(blueprint, token):
             user = query.one()
             login_user(user)
         except NoResultFound:
-            pass
+            # TODO Warn user on website that account is not known
+            print('Unknown Github user {}'.format(username))
     return redirect(url_for('realtek.index'))
 
 